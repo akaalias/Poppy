70f0389672bff2bc1a72e14984553753d8b4590b
Alexis Rondeau <alexis.rondeau@gmail.com>
2023-05-27 16:58:34 +0200

    Updates URL Bar

--- Poppy/WebView.swift
+++ Poppy/WebView.swift
@@ -9,44 +9,73 @@ import SwiftUI
 import WebKit
 
 struct WebViewWithUrlBar: View {
+    var mouseLocation: NSPoint { NSEvent.mouseLocation }
+    
     @State private var urlString = ""
     @State private var isHovering = false
     @StateObject private var webViewStore = WebViewStore()
     @State var hovering = false
     @StateObject private var settings = Settings.shared
+    @State private var size: CGSize = .zero
+    @State private var distanceOfMouseFromTopOfWindowFrame: CGFloat = 0.0
     
     var body: some View {
-        VStack {
-            HStack {
-                TextField("Enter a URL", text: $urlString, onCommit: {
-                    if urlString.hasPrefix("https://") {
-                        webViewStore.loadUrl(urlString)
-                    }
-                })
-                .padding(.leading, 72)
-                .font(.body)
-                .textFieldStyle(.plain)
-                
-                Image(systemName: settings.isPinned ? "pin.fill" : "pin")
-                    .foregroundColor(Color("PinColor"))
-                    .onTapGesture {
-                        settings.isPinned.toggle()
-                    }
-                    .fixedSize()
-                    .scaledToFit()
-                    .frame(width: 12, height: 12)
-                
-                Spacer()
-            }
-            .padding(.top, 6)
-            .opacity(hovering ? 1.0 : 0.2)
-            .transition(.opacity)
-            .animation(.easeOut(duration: 0.3))
-            .onHover { hovering in
-                toggleVisibilityToggle()
-            }
-            
+        GeometryReader { geometry in
             WebView(webView: webViewStore.webView)
+                .ignoresSafeArea()
+                .background(.clear)
+            
+            .safeAreaInset(edge: .top, content: {
+                HStack(spacing: 0) {
+                    TextField("Enter a URL", text: $urlString, onCommit: {
+                        if !urlString.isEmpty {
+                            if  !urlString.hasPrefix("https://") {
+                                self.urlString = "https://" + self.urlString
+                            }
+                            webViewStore.loadUrl(urlString)
+                        }
+                    })
+                    .padding(.leading, 72)
+                    .font(.body)
+                    .textFieldStyle(.plain)
+                    .foregroundColor(Color("URLBarText"))
+                    
+                    Image(systemName: settings.isPinned ? "pin.fill" : "pin")
+                        .foregroundColor(Color("PinColor"))
+                        .onTapGesture {
+                            settings.isPinned.toggle()
+                        }
+                        .fixedSize()
+                        .scaledToFit()
+                        .frame(width: 12, height: 12)
+                    
+                    Spacer()
+                }
+                .padding(.top, 8)
+                .padding(.bottom, 8)
+                .frame(height: hovering ? 28 : 0)
+                .offset(y: hovering ? 0 : -10)
+                .background(Color("URLBarColor"))
+                .animation(.easeOut(duration: 0.3))
+                .onChange(of: self.distanceOfMouseFromTopOfWindowFrame) { newValue in
+                    if newValue > 30 {
+                        hovering = false
+                    } else {
+                        hovering = true
+                    }
+                }
+            })
+            .onAppear {
+                NSEvent.addLocalMonitorForEvents(matching: [.mouseMoved]) { event in
+                    self.distanceOfMouseFromTopOfWindowFrame = size.height - event.locationInWindow.y
+                    return event
+                }
+                size = geometry.size
+                
+                self.hovering = true
+            }.onChange(of: geometry.size) { newSize in
+                size = newSize
+            }
         }
     }
     
@@ -75,5 +104,6 @@ struct WebView: NSViewRepresentable {
     }
     
     func updateNSView(_ nsView: WKWebView, context: Context) {
+        nsView.setValue(false, forKey: "drawsBackground")
     }
 }
