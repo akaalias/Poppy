cf07af8b98094c59aa5da365c51cb05f6d04f10e
Alexis Rondeau <alexis.rondeau@gmail.com>
2023-05-30 20:37:00 +0200

    Bumps to 1.1.7

--- Poppy/TuckedView.swift
+++ Poppy/TuckedView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct TuckedView: View {
     @StateObject private var state = AppState.shared
-    
+    @State var hovering = false
+
     var body: some View {
-            Image(systemName: "chevron.left")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 14, height: 14)
-            .foregroundColor(Color("PinColor"))
+        Image(systemName: "chevron.left")
+        .foregroundColor(Color("PinColor"))
+        .background(Circle()
+            .fill(Color("URLBarColor"))
+            .frame(width: 100, height: 100)
+        )
+        .opacity(self.hovering ? 1 : 0.5)
+        .transition(.opacity)
+        .animation(.easeOut)
+        .onHover(perform: { hover in
+            self.hovering = hover
+        })
     }
 }
 
