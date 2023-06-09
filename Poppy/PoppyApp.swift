167e2b564be965ec2e4224cb3fb09e2c7a311823
Alexis Rondeau <alexis.rondeau@gmail.com>
2023-06-09 09:00:18 +0200

    New version test

--- Poppy/PoppyApp.swift
+++ Poppy/PoppyApp.swift
@@ -17,8 +17,6 @@ struct PoppyApp: App {
     @StateObject private var state = AppState.shared
     
     init() {
-        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
-        // This is where you can also pass an updater delegate if you need one
         updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
     }
     
